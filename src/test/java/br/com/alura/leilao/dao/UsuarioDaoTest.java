b9d59f11237e724627a8973f9344ac441d0e597e
Sarah Tumenas <sarah.marques@spread.com.br>
2022-06-20 10:14:50 -0300

    Refatorando os testes

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -3,6 +3,8 @@ package br.com.alura.leilao.dao;
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.util.JpaUtil;
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -13,19 +15,23 @@ class UsuarioDaoTest {
 
     private UsuarioDao dao;
 
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = new JpaUtil().getEntityManager();
+        this.dao = new UsuarioDao(this.em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
 
     @Test
     void deveriaEncontrarUsuarioCadastrado() {
-
-        EntityManager em = JpaUtil.getEntityManager();
-
-        this.dao = new UsuarioDao(em);
-
-        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
-
+        Usuario usuario = criarUsuario();
         Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
         Assert.assertNotNull(encontrado);
 
@@ -33,17 +39,14 @@ class UsuarioDaoTest {
 
     @Test
     void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+        criarUsuario();
+       Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
 
-        EntityManager em = JpaUtil.getEntityManager();
+    }
 
-        this.dao = new UsuarioDao(em);
-
+    private Usuario criarUsuario () {
         Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-        em.getTransaction().begin();
         em.persist(usuario);
-        em.getTransaction().commit();
-
-        Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
-
+        return usuario;
     }
 }
